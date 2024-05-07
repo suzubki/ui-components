ce6bf029d957cbed1c1851aa36b86fc420486cb6
Darwin_N19 <darwinccn25@gmail.com>
2024-05-07 12:39:49 -0500

    setup lint staged

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,40 @@
+import react from "eslint-plugin-react";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-
+import reactRecommended from "eslint-plugin-react/configs/recommended.js";
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReactConfig,
+  {
+    ignores: ["dist/**/*"],
+  },
+  {
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    ignores: ["dist/**/*"],
+    ...reactRecommended,
+    settings: {
+      version: "detect",
+    },
+    languageOptions: {
+      ...reactRecommended.languageOptions,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+      react,
+    },
+    rules: {
+    },
+  },
 ];
