55e9f05d00a78d8edafcd9cbe059fcf8a068c2f1
Darwin_N19 <darwinccn25@gmail.com>
2024-05-07 16:48:20 -0500

    added storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,12 +2,14 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:storybook/recommended"
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: [
+    'dist', '.eslintrc.cjs', "tailwind.config.js"],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: 'detect' } },
   plugins: ['react-refresh'],
