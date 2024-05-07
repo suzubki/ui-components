3b63fd179c4bd28dbb79fcc93d786676511d4435
Darwin_N19 <darwinccn25@gmail.com>
2024-05-07 16:52:57 -0500

    added relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
+import path from "path"
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
@@ -10,5 +11,10 @@ export default defineConfig({
   ],
   test: {
     environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
